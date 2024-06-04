e268bfb0b00fb2b4dda6ef48eadde7fe6ade8d21
CrisSal2 <salgado.chris.m@gmail.com>
2024-06-03 20:31:33 -0700

    added questions

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [title, description, features, usage, email, github];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    inquirer
+    .prompt ([
+        {
+            type: 'input',
+            messege: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            messege: 'Give a description of your project:',
+            name: 'description',
+        },
+        {
+            type: 'input',
+            messege: 'What are some of the features of your project?',
+            name: 'features',
+        },
+        {
+            type: 'input',
+            messege: 'What are some of the usages of your project?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            messege: 'What email can people use to contact you?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            messege: 'What is github username?',
+            name: 'github',
+        },
+        
+    ])
+}
 
 // TODO: Create a function to initialize app
 function init() {}
